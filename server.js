038454a438374b21c4a7c7ec5927bf32a44b5857
Aecop <joonruiz08@gmail.com>
2023-01-19 16:51:30 -0800

    routing has been created.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const apiRoute = require('./routes/apiRoute/noteRoute');
-const htmlRoute = require('./routes/htmlRoute/index');
+const apiRoute = require('./routes/apiRoute/');
+const htmlRoute = require('./routes/htmlRoute/');
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
+//Parse JSON
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('public'))
+
+//use api route
 app.use('/api', apiRoute);
 app.use('/', htmlRoute);
 
